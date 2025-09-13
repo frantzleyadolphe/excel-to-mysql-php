399e363ea7fe78357aafb6d10b19c28e8980d4d7
frantzleyadolphe <dolphdjyangelo@gmail.com>
2025-09-12 23:54:53 -0400

    feat : importer ak jere data nan fichye excel to Mysql

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
 <title>Excel to MySQL</title>
 <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-red-900 p-6">
+<body class="bg-slate-900 p-6">
     <div class="max-w-3xl mx-auto bg-white p-6 rounded shadow relative">
         <h1 class="text-2xl font-bold mb-4 text-center">Upload done nan fichye excel pou mete nan database MySQL</h1>
 
@@ -25,9 +25,9 @@
                 hover:file:bg-red-900 hover:file:text-white" required>
 
             <input type="text" name="table_name" placeholder="Non tab la pou mete done yo" class="w-full p-2 border rounded" required>
-            <input type="text" name="unique_key" placeholder="kle kolon inik lan (opsyonèl)" class="w-full p-2 border rounded">
+            <input type="text" name="unique_key" placeholder="kle kolon inik lan" class="w-full p-2 border rounded" required>
 
-            <button type="submit" class="bg-red-600 text-white px-4 py-2 rounded hover:bg-black">Kòmanse Import</button>
+            <button type="submit" class="bg-red-600 text-white px-4 py-2 rounded w-full hover:bg-black">Lanse enpotasyon</button>
         </form>
 
         <!-- Filter logs -->
@@ -43,8 +43,8 @@
 
         <!-- Progress bar -->
         <div class="mt-4">
-            <div class="w-full bg-gray-200 rounded h-4 overflow-hidden">
-                <div id="progressBar" class="bg-red-500 h-4 w-0 transition-all duration-500"></div>
+            <div class="w-full bg-gray-200 rounded h-5 overflow-hidden">
+                <div id="progressBar" class="bg-green-900 h-5 w-0 transition-all duration-500"></div>
             </div>
             <div id="logs" class="mt-4 h-64 overflow-y-auto p-2 border rounded bg-gray-50"></div>
         </div>
